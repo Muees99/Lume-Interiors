92661de50088c307b6c1f375b5b0267e90c3b66d
Muees99 <132849693+Muees99@users.noreply.github.com>
2025-10-14 11:11:27 +0100

    Footer

--- libs/constants.ts
+++ libs/constants.ts
@@ -10,22 +10,14 @@ export const NAVLINKS: NavLinkProps[] = [
 ];
 
 export const FOOTER_LINKS: FooterLinkProps = {
-  company: [
-    { id: 1, link: "", label: "about us" },
-    { id: 2, link: " tel:", label: "our mission" },
-    { id: 3, link: "", label: "career" },
-    { id: 4, link: "", label: "contact" },
+  ourServices: [
+    { id: 1, link: "", label: "architecture" },
+    { id: 2, link: " tel:", label: "Interior Design" },
+    { id: 3, link: "", label: "Home Decor" },
+    { id: 4, link: "", label: "Urban Design" },
+    { id: 5, link: "", label: "Design System" },
   ],
-  services: [
-    { id: 1, link: "", label: "advocacy" },
-    { id: 2, link: "", label: "education" },
-    { id: 3, link: "", label: "care" },
-  ],
-  resources: [
-    { id: 1, link: "", label: "blog" },
-    { id: 2, link: "", label: "FAQ" },
-    { id: 3, link: "", label: "support" },
-  ],
+  
   link: "",
   label: "",
 };
