7b52440d5e31a2b4efa63ef3404d2462cc13e842
Muees99 <132849693+Muees99@users.noreply.github.com>
2025-11-05 10:04:24 +0100

    CartPage

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,9 @@ export default function NotFound() {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen bg-[#f9f6f3] text-gray-700 text-center px-4">
       <h1 className="text-5xl font-cinzel mb-4">404</h1>
-      <p className="text-lg mb-8">The page you’re looking for doesn’t exist.</p>
+      <p className="text-lg mb-8">
+        The page you&apos;re looking for doesn&apos;t exist.
+      </p>
       <Link
         href="/"
         className="bg-[#B98E75] text-white px-6 py-3 rounded-full hover:bg-[#a47a64] transition"
